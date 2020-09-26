e6180110b24bec7d7c99a894a6280483b3176a23
Nicolas PASCAL <np.pascal@gmail.com>
2020-09-26 09:27:06 +0200

    Create svg_metadata.py

---
+++ filer/utils/svg_metadata.py
@@ -0,0 +1,10 @@
+from svglib.svglib import svg2rlg
+
+def get_metadata_for_svg(file_obj):
+    try:
+        drawing = svg2rlg(file_obj.file)
+        _width, _height = drawing.width, drawing.height
+        _bounds = [left, bottom, right, top] =  drawing.getBounds()
+    except:
+        _width, _height , _bounds = None, None, None
+    return  _width, _height, _bounds
