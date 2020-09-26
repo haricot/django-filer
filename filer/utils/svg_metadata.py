a76a171a590b3fcfd9ae0da09e800bb8257a2c40
Nicolas PASCAL <np.pascal@gmail.com>
2020-09-26 10:10:40 +0200

    Update svg_metadata.py

--- filer/utils/svg_metadata.py
+++ filer/utils/svg_metadata.py
@@ -6,6 +6,6 @@ def get_metadata_for_svg(file_obj):
         drawing = svg2rlg(file_obj.file)
         _width, _height = drawing.width, drawing.height
         _bounds = [left, bottom, right, top] =  drawing.getBounds()
-    except:
-        _width, _height , _bounds = None, None, None
+    except Exception:
+        _width, _height, _bounds = None, None, None
     return  _width, _height, _bounds
