a0ccac3befdb7e81ab1818cac7bc3028727ea706
Nicolas PASCAL <np.pascal@gmail.com>
2020-09-26 11:17:23 +0200

    Update test_admin.py

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -512,7 +512,7 @@ class BulkOperationsMixin:
 
     def create_image(self, folder, filename=None):
         filename = filename or 'test_image.jpg'
-        mime_type = filename.mime_type or 'image/jpeg'
+        mime_type = 'image/jpeg'
         file_obj = django.core.files.File(open(self.filename, 'rb'), name=filename)
         image_obj = Image.objects.create(owner=self.superuser, original_filename=self.image_name, file=file_obj, folder=folder, content_type=mime_type)
         image_obj.save()
