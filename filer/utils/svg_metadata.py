20eaf1e57501ac5808dc9af7ff37966f0d915281
Nicolas PASCAL <np.pascal@gmail.com>
2020-09-30 17:24:05 +0200

    refactor get_metadata_for_svg: round bounds

--- filer/utils/svg_metadata.py
+++ filer/utils/svg_metadata.py
@@ -1,11 +1,15 @@
 from svglib.svglib import svg2rlg
 
+def round_bounds(drawing):
+     left, bottom, right, top = drawing.getBounds()
+     return round(left, 1), round(bottom, 1), round(right, 1), round(top, 1)
 
-def get_metadata_for_svg(file_obj):
+def get_metadata_for_svg(file_obj, drawing=None):
     try:
         drawing = svg2rlg(file_obj.file)
-        _width, _height = drawing.width, drawing.height
-        _bounds = [left, bottom, right, top] = drawing.getBounds()
     except Exception:
-        _width, _height, _bounds = None, None, None
-    return _width, _height, _bounds
+        _width, _height , _bounds = None, None, None
+    if drawing:
+        _width, _height = drawing.width, drawing.height
+        _bounds = round_bounds(drawing)
+    return  _width, _height, _bounds
